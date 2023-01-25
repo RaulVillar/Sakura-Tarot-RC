570b246d5806f37b238ae778e765fa449b47c9d5
Raul Villar <raultheviking@gmail.com>
2023-01-25 15:25:08 +0100

    uwu

--- sakuraapp/src/Components/SakuraCards.jsx
+++ sakuraapp/src/Components/SakuraCards.jsx
@@ -6,7 +6,6 @@ let sakuraUrl = " https://6388b6e5a4bb27a7f78f96a5.mockapi.io/sakura-cards/";
 
 export class Api extends React.Component {
 
-	// Constructor
 	constructor(props) {
 		super(props);
 
@@ -16,8 +15,6 @@ export class Api extends React.Component {
 		};
 	}
 
-	// ComponentDidMount is used to
-	// execute the code
 	componentDidMount() {
 		fetch(sakuraUrl)
 			.then((response) => response.json())
@@ -25,24 +22,23 @@ export class Api extends React.Component {
 				this.setState({
 					items: json,
 					DataisLoaded: true
-				
-					
 				});
 			})
 	}
 	render() {
 		const { DataisLoaded, items } = this.state;
-		if (!DataisLoaded) return 
+		if (!DataisLoaded) return
 		<div>
-			<h1> Please wait some time.... </h1> 
-		</div> ;
+			<h1> Please wait some time.... </h1>
+		</div>;
 
 		return (
-		<div className = "App">
-			<h2> Las cartas del fuuutuuurooooo </h2>
-			{	
-			shuffleArray(items).map((item) => (
-					<img key={item[0].id} class="cards-img" src={item[0].sakuraCard}></img>))}	
-		</div>
-		);}
+			<div className="App">
+				<h2> Las cartas del fuuutuuurooooo </h2>
+				{
+					shuffleArray(items).map((item) => (
+						<img key={item[0].id} class="cards-img" src={item[0].sakuraCard}></img>))}
+			</div>
+		);
 	}
+}
