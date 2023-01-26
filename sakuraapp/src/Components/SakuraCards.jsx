6520dba92e1edc8393871a889670f2a374c7af40
ChristianCarbajo <christian_carbajo@hotmial.com>
2023-01-26 18:52:18 +0100

    added meaning

--- sakuraapp/src/Components/SakuraCards.jsx
+++ sakuraapp/src/Components/SakuraCards.jsx
@@ -13,7 +13,8 @@ export class Api extends React.Component {
 
 		this.state = {
 			items: [],
-			DataisLoaded: false
+			DataisLoaded: false,
+			
 		};
 	}
 
@@ -23,20 +24,22 @@ export class Api extends React.Component {
 			.then((json) => {
 				this.setState({
 					items: json,
-					DataisLoaded: true
+					DataisLoaded: true,
+					
 				});
 			})
 	}
 
 	
 	render() {
-		const { DataisLoaded, items } = this.state;
+		const { DataisLoaded, items} = this.state;
 		if (!DataisLoaded) return
 		<div>
 			<h1> Please wait some time.... </h1>
 		</div>;
 
 		return (
+			
 			<div className="sakuraCards">
 				<Header />
 				<hr className="sakuraCards-line" />
@@ -44,21 +47,27 @@ export class Api extends React.Component {
 					<h2 className="sakuraCards-title">Pasado</h2>
 					{
 						shuffleArray(items).map((item) => (
-						<img onClick={() => document.getElementById (item[0].id).src=item[0].sakuraCard} id={item[0].id} key={item[0].id} className="cards-img" src={item[0].cardsReverse.sakuraReverse}></img>
-						))}
+						<img onClick={() =>{(document.getElementById (item[0].id).src=item[0].sakuraCard) && (document.getElementById("yourPast").innerHTML=item[0].meaning)}} id={item[0].id} key={item[0].id} className="cards-img" src={item[0].cardsReverse.sakuraReverse}></img>
+					  ))}
+						<h2 id="yourPast"></h2>
 				</div>
 				<div>
 					<h2 className="sakuraCards-title">Presente</h2>
 					{
 						shuffleArray(items).map((item) => (
-						<img onClick={() => document.getElementById (item[0].id).src=item[0].sakuraCard} id={item[0].id} key={item[0].id} className="cards-img" src={item[0].cardsReverse.sakuraReverse}></img>))}
+							
+						<img onClick={() => {(document.getElementById (item[0].spanishName).src=item[0].sakuraCard) && (document.getElementById("yourPresent").innerHTML=item[0].meaning)}} id={item[0].spanishName} key={item[0].id} className="cards-img" src={item[0].cardsReverse.sakuraReverse}></img>		
+						))}
+					<h2 id="yourPresent"></h2>	
 				</div>
 				<div>
 					<h2 className="sakuraCards-title">Futuro</h2>
 					{
 						shuffleArray(items).map((item) => (
-						<img onClick={() => document.getElementById (item[0].id).src=item[0].sakuraCard} key={item[0].id} className="cards-img" src={item[0].cardsReverse.sakuraReverse}></img>))}
+						<img onClick={() => (document.getElementById (item[0].englishName).src=item[0].sakuraCard) && (document.getElementById("yourFuture").innerHTML=item[0].meaning)} id={item[0].englishName} key={item[0].id} className="cards-img" src={item[0].cardsReverse.sakuraReverse}></img>))}
+						<h2 id="yourFuture"></h2>
 				</div>
+				
 				<Footer />
 			</div>
 		);
