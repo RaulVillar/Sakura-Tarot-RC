c0627513c74d0b4fc07a1ccf8939bbd7edbe322c
Raul Villar <raultheviking@gmail.com>
2023-01-26 14:24:11 +0100

    Added onClick

--- sakuraapp/src/Components/Random.jsx
+++ sakuraapp/src/Components/Random.jsx
@@ -3,13 +3,11 @@ import React from 'react'
 export function shuffleArray(arr){
 		
     const copy = [...arr];
-   
     const output =[] ;
     
     while (output.length<5) {
        
-         output.push(copy.splice(Math.floor(Math.random() * copy.length), 1)) ;
+         output.push(copy.splice(Math.round(Math.random() * copy.length), 1)) ;
     }
-    
     return output;
 };
